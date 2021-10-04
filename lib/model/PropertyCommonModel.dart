8b47bb8f17050d8d743d8df6b07ccbdcae5f4943
elie <elibarabarquez.dev@mail.com>
2021-10-04 14:28:15 +0800

    initial commit

---
+++ lib/model/PropertyCommonModel.dart
@@ -0,0 +1,71 @@
+class CategoryFormModel {
+  String categoryid;
+  String popsid;
+  String title;
+  bool condition_brandnew;
+  bool condition_likebrandnew;
+  bool condition_wellused;
+  bool condition_heavilyused;
+  bool condition_new;
+  bool condition_preselling;
+  bool condition_preowned;
+  bool condition_foreclosed;
+  bool condition_used;
+  bool priceselect_forsale;
+  bool priceselect_forfree;
+  double priceinput_price;
+  String description;
+  bool ismoreandsameitem;
+  bool deal_meetup;
+  bool deal_delivery;
+  String brandCODE;
+  String location_cityproviceCODE;
+  String location_streetaddress;
+  double unitdetails_lotarea;
+  String unitdetails_termsCODE;
+  int unitdetails_bedroom;
+  int unitdetails_bathroom;
+  double unitdetails_floorarea;
+  int unitdetails_parkingspace;
+  bool unitdetails_furnish_unfurnish;
+  bool unitdetails_furnish_semifurnish;
+  bool unitdetails_furnish_fullyfurnish;
+  bool unitdetails_room_private;
+  bool unitdetails_room_shared;
+
+  CategoryFormModel({
+    this.categoryid,
+    this.popsid,
+    this.title,
+    this.condition_brandnew,
+    this.condition_likebrandnew,
+    this.condition_wellused,
+    this.condition_heavilyused,
+    this.condition_new,
+    this.condition_preselling,
+    this.condition_preowned,
+    this.condition_foreclosed,
+    this.condition_used,
+    this.priceselect_forsale,
+    this.priceselect_forfree,
+    this.priceinput_price,
+    this.description,
+    this.ismoreandsameitem,
+    this.deal_meetup,
+    this.deal_delivery,
+    this.brandCODE,
+    this.location_cityproviceCODE,
+    this.location_streetaddress,
+    this.unitdetails_lotarea,
+    this.unitdetails_termsCODE,
+    this.unitdetails_bedroom,
+    this.unitdetails_bathroom,
+    this.unitdetails_floorarea,
+    this.unitdetails_parkingspace,
+    this.unitdetails_furnish_unfurnish,
+    this.unitdetails_furnish_semifurnish,
+    this.unitdetails_furnish_fullyfurnish,
+    this.unitdetails_room_private,
+    this.unitdetails_room_shared,
+  });
+}
